0053c531786a801981f6606088f5ebd460fc8843
Lương Văn Tiến <luongvantien1194@gmail.com>
2020-07-29 15:20:11 +0700

    Categories seeder

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+    	'name', 'slug', 'parent_id'
+    ];
+    
+    public function childs()
+    {
+    	return $this->hasMany('App\Categoy', 'parent_id', 'id');
+    }
 }
