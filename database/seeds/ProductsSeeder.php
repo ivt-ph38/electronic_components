3b7e500f1450049f4f17a3b27b0cfdc5a777b2c7
tranphuchieu <phuchieu25897@gmail.com>
2020-08-01 15:18:29 +0700

    create product seeder

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -21,5 +21,6 @@ class ProductsSeeder extends Seeder
    			['name' => 'Mạch karaoke AD828', 'slug' => 'mach-karae-pt2399-ad828', 'description' => 'Chip: PT2399 + AD828 | Điện áp làm việc: 6 ~ 15VAC / 7 ~ 24VDC', 'image' => 'mach-karaoke-pt2399-ad828', 'price' => '100000', 'discount' => '10', 'quantity' => '100', 'category_id' => 5, 'status' => 1, 'detail' => 'Mạch karaoke PT2399 AD828 sử dụng chip PT2399 chuyên dụng cho mạch karaoke, chip hoạt động với hiệu suất ổn định, khuếch đại micro sử dụng chip AD828. Nguồn cung cấp linh hoạt, có thể sử dụng nguồn DC hoặc AC.'], 
 
         ];
+    	Product::Insert($data);
     }
 }
