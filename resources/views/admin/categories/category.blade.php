67b9b94e9d70d697109533d3937ba6fc5e97daa8
Lương Văn Tiến <luongvantien1194@gmail.com>
2020-08-21 11:06:34 +0700

    validate categories

--- resources/views/admin/categories/category.blade.php
+++ resources/views/admin/categories/category.blade.php
@@ -6,22 +6,25 @@
 							@endfor
 							{{ $category->name }}
 						</td>
+						<?php 
+							$count = 0;
+							$count += count($category->products);
+							foreach ($category->childs as $item) {
+								$count += count($item->products);
+							}
+						?>
+						<td>{{$count}}</td>
 						<td class="actions">	
 							<a href="{{ route('admin.categories.edit', [$category->id]) }}" class="btn btn-secondary" title="{{ __('Chỉnh Sửa') }}"><i class="far fa-edit">Chỉnh Sửa</i></a>
 						</td>
 						<td>
-							{{-- @php
-								$messagedel = 'Bạn muốn xóa danh mục '.$category->name;
-							@endphp --}}
-
-							{{-- {{ Form::button('<i class="far fa-times-circle"></i>', ['type' => 'submit', 'class' => 'btn btn-danger','name' => 'delete', 'value' => $category->id, 'onclick' => "return confirm('$messagedel')"]) }}
-		 					--}}				
-
+						<?php if ($count == 0) {?>		
 		 					<form action="{{ route('admin.categories.delete', [$category->id]) }}" method="POST" role="form">			
-		 						@method('DELETE')
-		 					 @csrf
-		 					 <button type="submit" class="btn btn-danger"><i class="fad fa-trash" onclick="return confirm('Bạn có chắc muốn xóa '.$category->name)">Xoá</i></button>
+		 					@method('DELETE')
+		 					@csrf
+		 					<button type="submit" class="btn btn-danger"><i class="fad fa-trash" onclick="return confirm('Bạn có chắc muốn xóa '.$category->name)">Xoá</i></button>
 
 							</form>
+						<?php } ?>
 						</td>
 					</tr>
